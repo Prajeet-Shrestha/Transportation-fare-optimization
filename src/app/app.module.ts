c17db69454dba2c320773c9f9aaf6206e4044b6e
SaahilMahato <saahilmahato72@gmail.com>
2020-11-03 19:56:45 +0545

    removed unecessary imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AgmCoreModule } from "@agm/core";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
